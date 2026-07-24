d3b5bdd7ebfc98fb28764693cfb14940d6da89e5
robertm11-arch <svetlana.s9@tumo.org>
2026-07-24 20:13:26 +0400

    js changes

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let mainGame = document.querySelector('.game-Block'),
+gameWraper = document.querySelector('.game-wrap'),
+startBtn = document.querySelector('start-btn'),
+endBtm = document.querySelector('.end-Btn'),
+btnAnswers = document.querySelectorAll('.answer'),
+blockQuestion = document.querySelectorAll('.question'),
+helpBtns = document.querySelectorAll('.hets-help'),
+winsBlock = document.querySelector('.wins-block'),
+helpFifty = document.querySelector('.fifty-fifty'),
+helpHall = document.querySelector('.hall-help'),
+helpFriends = document.querySelector('.qall-friend'),
+helpAI = document.querySelector('.ai-help'),
+game = document.querySelector(',game')
+//////////===========
+
+let changetQuestion = document.getElementById('hitBox')
+let extraQuestion = document.getElementById('extra')
+let flagExtra = true
+
+
